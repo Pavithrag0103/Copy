8a65ee5b96995945c3b36b6129771744b9bb0c05
Padilla Santos <cesar.padillasantos@syneoshealth.com>
2025-07-10 08:44:18 -0600

    removed unnecesary files

--- data/intake/create-intake.ts
+++
@@ -1,5 +0,0 @@
-export const CreateIntakeTestData = {
-  projectCode: '7062663',
-  opportunityName: '7062663-Pfizer Inc.-9200 - Early Phase-Pfizer-CP&B-SAB-C3421063-PF06882961-Hum',
-  sponsorName: 'Pfizer Inc.',
-};
